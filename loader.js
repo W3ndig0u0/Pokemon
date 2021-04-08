851acd0e1126d09d8b1b7527737b4c9e92c2add4
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-08 22:26:44 +0200

    loading

--- loader.js
+++ loader.js
@@ -1,12 +1,14 @@
-
 var loader = document.querySelector(".loader");
+var myVar;
 
-
-document.addEventListener('readystatechange', event => { 
-
-  if (event.target.readyState === "complete") {
-  // !starta när allt är redo
-  loader.className += " hidden";
-  console.log("Start");
+function timer() {
+  myVar = setTimeout(showPage, 2000);
 }
-});
+
+function showPage() {
+  console.log("Start");  
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  document.getElementById("footer").style.display = "block";
+}
