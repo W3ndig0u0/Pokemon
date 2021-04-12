6bdf9183c777a0cdadf435ec8ed245b2bb5c24d5
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-12 10:40:08 +0200

    scrollbar be GONE

--- header.js
+++ header.js
@@ -1,5 +1,6 @@
 [document.querySelector('.menu'), document.querySelector('.menu1')].forEach(item => {  
   item.addEventListener('click', event => {
     document.querySelector(".header").classList.toggle("active");
+    document.querySelector("body").classList.toggle("active");
   })
 })
--- loader.js
+++ loader.js
@@ -1,11 +1,13 @@
 function timer() {
- var myVar = setTimeout(showPage, 3000);
+  document.querySelector("body").classList.toggle("active");
+ var myVar = setTimeout(showPage, 3500);
 }
 
 function showPage() {
   console.log("Start");  
   document.getElementById("loader").style.display = "none";
-  document.getElementById("loader").style.display = "none";
   document.getElementById("main").style.display = "block";
   document.getElementById("footer").style.display = "block";
+  document.querySelector("body").classList.toggle("active");
+
 }
