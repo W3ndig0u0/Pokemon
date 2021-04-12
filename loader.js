51ff3e27b36b6ef3f4f7768ead3f76863ec14283
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-12 10:42:09 +0200

    animation fix

--- loader.js
+++ loader.js
@@ -1,10 +1,9 @@
 function timer() {
-  document.querySelector("body").classList.toggle("active");
+ document.querySelector("body").classList.toggle("active");
  var myVar = setTimeout(showPage, 3500);
 }
 
 function showPage() {
-  console.log("Start");  
   document.getElementById("loader").style.display = "none";
   document.getElementById("main").style.display = "block";
   document.getElementById("footer").style.display = "block";
